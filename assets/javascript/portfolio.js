17ce67a7ce814639aa607f635defc5f4a4fc2cb4
DMilligan <dmilligan@springlocker.com>
2018-05-17 16:01:13 -0400

    added styling to portfolio tiles

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -5,3 +5,7 @@ $(".portfolio-image").on("mouseenter mouseleave touchstart", function(e) {
 
     $(this).toggleClass("tile-hover");
 });
+
+$(".portfolio-image").on("touchleave", function(e) {
+    $(this).toggleClass("tile-hover");
+});
