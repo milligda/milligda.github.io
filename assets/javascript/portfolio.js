c6d13077683a47c68cc583cf59818e637cb4f896
DMilligan <dmilligan@springlocker.com>
2018-11-15 12:31:49 -0500

    replacing the old site with the new portfolio

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,11 +1,56 @@
-$(".portfolio-image").on("mouseenter mouseleave touchstart", function(e) {
-    if(e.type == 'touchstart') {
-        $(this).off('mouseenter mouseleave');
+(function($) {
+
+  /**
+   * Copyright 2012, Digital Fusion
+   * Licensed under the MIT license.
+   * http://teamdf.com/jquery-plugins/license/
+   *
+   * @author Sam Sehnert
+   * @desc A small plugin that checks whether elements are within
+   *     the user visible viewport of a web browser.
+   *     only accounts for vertical position, not horizontal.
+   */
+
+  $.fn.visible = function(partial) {
+    
+      var $t            = $(this),
+          $w            = $(window),
+          viewTop       = $w.scrollTop(),
+          viewBottom    = viewTop + $w.height(),
+          _top          = $t.offset().top,
+          _bottom       = _top + $t.height(),
+          compareTop    = partial === true ? _bottom : _top,
+          compareBottom = partial === true ? _top : _bottom;
+    
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+  };
+    
+})(jQuery);
+
+/**
+   * Based on the CSS-Tricks article:
+   * Slide In (as you scroll down) Boxes
+   * https://css-tricks.com/slide-in-as-you-scroll-down-boxes/
+   *
+   * @author Chris Coyier
+   */
+
+var win = $(window);
+var allMods = $(".portfolio-item-container");
+
+allMods.each(function(i, el) {
+  var el = $(el);
+  if (el.visible(true)) {
+    el.addClass("already-visible");
+  }
+})
+
+win.scroll(function(event) {
+  allMods.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in");
     }
-
-    $(this).toggleClass("tile-hover");
-});
-
-$(".portfolio-image").on("touchend click", function(e) {
-    $(this).toggleClass("tile-hover");
+  });
 });
